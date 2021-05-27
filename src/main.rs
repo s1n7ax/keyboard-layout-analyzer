01041c6094251115e2743571b74ef546d4d7030d
s1n7ax <srineshnisala@gmail.com>
2021-05-27 23:01:31 +0530

    Add feat log report

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,16 @@ fn main() {
     KeyLogger::log(&mut key_logger, &'h');
     KeyLogger::log(&mut key_logger, &'h');
     KeyLogger::log(&mut key_logger, &'g');
+    KeyLogger::log(&mut key_logger, &'a');
+    KeyLogger::log(&mut key_logger, &'s');
+    KeyLogger::log(&mut key_logger, &'d');
 
-    key_logger
-        .finger_movements_map
-        .iter()
-        .for_each(|val| println!("{:?}", val))
+    let mut report = LogReport::new();
+    report.add_logger(String::from("QWERTY"), key_logger);
+    report.print();
 }
 
+#[derive(Debug)]
 struct Key {
     value: char,
     finger: u8,
@@ -80,13 +83,14 @@ impl KeyboardBuilder {
     }
 }
 
+#[derive(Debug)]
 struct KeyLogger {
     keyboard: HashMap<char, Key>,
 
     finger_movements_map: HashMap<(i8, i8), u8>,
     finger_usage_map: HashMap<u8, u8>,
 
-    prev_finger: i8,
+    prev_finger: Option<u8>,
     prev_char: char,
     same_finger_usage: u8,
 }
@@ -99,7 +103,7 @@ impl KeyLogger {
             finger_movements_map: HashMap::new(),
             finger_usage_map: HashMap::new(),
 
-            prev_finger: -1,
+            prev_finger: Option::None,
             prev_char: '\0',
             same_finger_usage: 0,
         }
@@ -117,10 +121,149 @@ impl KeyLogger {
                 Entry::Vacant(v) => *v.insert(1u8),
             };
 
-            if self.prev_finger == key.finger as i8 && self.prev_char != key.value {
+            if !self.prev_finger.is_none()
+                && self.prev_finger.unwrap() == key.finger
+                && self.prev_char != key.value
+            {
                 self.same_finger_usage += 1;
             }
+
+            self.prev_finger = Option::Some(key.finger);
+            self.prev_char = key.value;
         };
+    }
+}
+
+struct LogReport {
+    key_loggers: HashMap<String, KeyLogger>,
+    row_headers: Vec<String>,
+    movement_header_map: Vec<((i8, i8), String)>,
+}
+
+impl LogReport {
+    fn new() -> Self {
+        let movement_header_map: Vec<((i8, i8), String)> = vec![
+            ((0, 0), "No Movement".to_string()),
+            ((0, 1), "Up Movement".to_string()),
+            ((0, -1), "Down Movement".to_string()),
+            ((1, 0), "Right Movement".to_string()),
+            ((-1, 0), "Left Movement".to_string()),
+            ((1, 1), "Top Right Movement".to_string()),
+            ((-1, 1), "Top Left Movement".to_string()),
+            ((1, -1), "Bottom Right Movement".to_string()),
+            ((-1, -1), "Bottom Left Movement".to_string()),
+        ];
+
+        let mut row_headers: Vec<String> = vec![
+            "Finger Movements".to_string(),
+            "Same Finger Usage".to_string(),
+        ];
+
+        row_headers.append(
+            &mut movement_header_map
+                .iter()
+                .map(|(_, header)| header.to_owned())
+                .collect(),
+        );
+
+        LogReport {
+            key_loggers: HashMap::new(),
+            row_headers,
+            movement_header_map,
+        }
+    }
+
+    fn add_logger(&mut self, name: String, logger: KeyLogger) {
+        self.key_loggers.insert(name, logger);
+    }
+
+    fn print(&self) {
+        let table_data = self.get_table_body_data();
+        self.print_table_headers(&self.get_table_header_data());
+        self.print_table_body(&self.row_headers, &table_data);
+    }
+
+    fn print_table_body(&self, row_headers: &Vec<String>, data_table: &Vec<Vec<u8>>) {
+        data_table.iter().enumerate().for_each(|(idx, row)| {
+            let mut str: String = String::from("");
+
+            str.push_str(&format!("{:<30}", row_headers[idx]));
+
+            row.iter()
+                .for_each(|row| str.push_str(&format!("{:<15}", row)));
+
+            println!("{}", str);
+        });
+    }
+
+    fn print_table_headers(&self, names: &Vec<String>) {
+        let mut str: String = String::new();
+
+        str.push_str(&format!("{:<30}", "CATEGORY"));
+
+        for name in names {
+            str.push_str(&format!("{:<15}", name));
+        }
+
+        println!("{}", str);
+    }
+
+    fn get_table_header_data(&self) -> Vec<String> {
+        self.key_loggers.keys().map(|key| key.to_owned()).collect()
+    }
+
+    fn get_table_body_data(&self) -> Vec<Vec<u8>> {
+        // rotate the matrix
+        let mut data_table: Vec<Vec<u8>> = Vec::new();
+        let logger_count = self.key_loggers.len();
+        let log_data = self.get_log_data_from_key_logger();
+
+        for cell_idx in 0..11 {
+            let mut row: Vec<u8> = Vec::with_capacity(logger_count);
+
+            for row_idx in 0..logger_count {
+                row.insert(row_idx, log_data[row_idx][cell_idx]);
+            }
+
+            data_table.push(row.to_vec());
+        }
+
+        return data_table;
+    }
+
+    fn get_log_data_from_key_logger(&self) -> Vec<Vec<u8>> {
+        self.key_loggers
+            .values()
+            .map(|logger| -> Vec<u8> {
+                let mut row: Vec<u8> = Vec::new();
+
+                // total finger movements
+                row.push(
+                    logger
+                        .finger_movements_map
+                        .iter()
+                        .filter(|(movement, _)| *movement != &(0i8, 0i8))
+                        .map(|(_, count)| count)
+                        .sum(),
+                );
+
+                // same finger usage
+                row.push(logger.same_finger_usage);
+
+                // individual finger movements
+                self.movement_header_map.iter().for_each(|(key, _)| {
+                    row.push(*logger.finger_movements_map.get(key).unwrap_or(&0u8))
+                });
+
+                return row;
+            })
+            .collect()
+    }
+}
+
+impl Default for LogReport {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
@@ -129,6 +272,7 @@ mod keyboard_tests {
 
     use super::KeyLogger;
     use super::KeyboardBuilder;
+    use super::LogReport;
     use std::collections::HashMap;
 
     fn get_key_logger() -> KeyLogger {
@@ -142,7 +286,7 @@ mod keyboard_tests {
     }
 
     #[test]
-    fn qwerty_finger_movement_validation_test() {
+    fn qwerty_finger_movement_validation() {
         let mut key_logger = get_key_logger();
 
         let mut key_sequence: HashMap<(i8, i8), Vec<char>> = HashMap::new();
@@ -173,7 +317,7 @@ mod keyboard_tests {
     }
 
     #[test]
-    fn qwerty_finger_usage_validation_test() {
+    fn qwerty_finger_usage_alphabet_validation() {
         let mut key_logger = get_key_logger();
         let str: &str = "qwertyuiop asdfghjkl; zxcvbnm,./";
         let expected_result = [3, 3, 3, 6, 6, 3, 3, 3];
@@ -182,9 +326,43 @@ mod keyboard_tests {
             key_logger.log(&char);
         });
 
-        key_logger.finger_usage_map.iter().for_each(|( finger, count)| {
-            let expected_count = expected_result.get(*finger as usize).unwrap();
-            assert_eq!(expected_count, count);
-        })
+        key_logger
+            .finger_usage_map
+            .iter()
+            .for_each(|(finger, count)| {
+                let expected_count = expected_result.get(*finger as usize).unwrap();
+                assert_eq!(expected_count, count);
+            })
+    }
+
+    #[test]
+    fn qwerty_finger_usage_word_validation() {
+        let word_expected_result_map = [
+            ("hello", [3, 1, 2, 2, 0, 0, 1, 0, 0, 0, 0]),
+            ("world", [3, 0, 2, 3, 0, 0, 0, 0, 0, 0, 0]),
+            ("back", [2, 0, 2, 0, 1, 0, 0, 0, 0, 1, 0]),
+            ("value", [3, 0, 2, 2, 1, 0, 0, 0, 0, 0, 0]),
+        ];
+
+        word_expected_result_map
+            .iter()
+            .for_each(|(word, expected_res)| {
+                println!("Testing the word::{}", word);
+
+                let mut key_logger = get_key_logger();
+
+                word.chars().for_each(|char| {
+                    &key_logger.log(&char);
+                });
+
+                let mut log_report = LogReport::new();
+                log_report.add_logger("QWERTY".to_string(), key_logger);
+
+                let actual_res = &log_report.get_log_data_from_key_logger()[0];
+
+                println!("{:?}\n{:?}", actual_res, expected_res);
+
+                assert!(Iterator::eq(actual_res.iter(), expected_res.iter()));
+            })
     }
 }
